9632df802a72fcaf79dbf888b2531011729db39f
Subrat Mishra <piyush.subrat8@gmail.com>
2023-09-02 02:49:45 +0530

    jsbasics

---
+++ CSS basics/discord/postcss.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+}
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
