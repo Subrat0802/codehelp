6c78a0a66ac3c13f7de496be77b0cb2164c8b7be
Subrat Mishra <piyush.subrat8@gmail.com>
2023-09-03 03:41:32 +0530

    day3 js

---
+++ Javascript/Js Basics/day3/index.js
@@ -0,0 +1,145 @@
+// console.log("Hello World");
+
+// let lastName = "babbar";
+
+
+// let firstName = new String("love");
+
+
+// let message = "this is my forst message";
+
+
+
+
+
+
+
+
+// let message1 = `this 
+// is my
+//  forst
+//   message
+//   ${lastName}`;
+
+//   console.log(message1);
+
+
+
+
+
+
+
+// //   Date 
+
+//   let date=new Date();
+//   console.log(date);
+
+
+
+
+
+
+
+
+
+  //Arrays
+
+
+  //insert
+
+//   let Number=[1,4,2,9];
+
+//   console.log(Number);
+
+//   Number.push(2)
+
+//   console.log(Number);
+
+//   //searching
+
+//searching primitive arrays
+
+//   console.log(Number.indexOf(4));
+
+//   console.log(Number.includes(9));
+
+//   Number.indexOf(4, 2);
+
+  
+
+
+
+//searching array of object index 
+
+// let courses=[
+//     {no:1, name:"rahul"},
+//     {no:2, name:"aman"}
+// ];
+
+// console.log(courses);
+
+// // let course = courses.find(function(course){
+// //     return course.name == 'aman';
+// // })
+
+// // console.log(course); 
+
+// // Arrow Function 
+// let course = courses.find(course => course.name == 'aman');
+
+// console.log(course); 
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Removing Element 
+
+
+// let Number=[1,2,3,5,4];
+
+// Number.pop(); //end
+// Number.shift(); //begain
+// Number.splice(2,1) //middil
+
+// console.log(Number)
+
+
+
+
+
+
+
+
+
+//empting array 
+
+// let numbers=[1,2,3,4,5];
+// let numbers2=numbers;
+// // numbers=[];
+// // numbers.length=0;
+// numbers.splice(0, numbers.length);
+
+// console.log(numbers);
+// console.log(numbers2)
+
+
+
+
+
+
+
+
+
+
+
+// Combining and slicing arrays: 
+
