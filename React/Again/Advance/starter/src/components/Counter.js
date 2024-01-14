e884cd2ff23e3d017282523bf97e9d7e4df895c4
Subrat Mishra <piyush.subrat8@gmail.com>
2024-01-14 22:04:13 +0530

    redux_inc_dec

---
+++ React/Again/Advance/starter/src/components/Counter.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { decrement, increment } from '../redux/slices/CounterSlice';
+
+const Counter = () => {
+    const count = useSelector((state) => state.counter.value);
+    const dispatch = useDispatch()
+  return (
+    <div>
+        <button onClick={() => dispatch(increment())}>Inc</button>    
+        <br/>
+        <div>{count}</div>
+        <br></br>
+        <button onClick={() => dispatch(decrement())}>Dec</button>
+    </div>
+  )
+}
+
+export default Counter
