2e468ca4265329dfe01aa99fc07b1578f3f6610a
Subrat Mishra <piyush.subrat8@gmail.com>
2024-02-01 16:21:47 +0530

    dev

--- MegaProject/server/config/database.js
+++ MegaProject/server/config/database.js
@@ -1,15 +1,3 @@
-const mongoose = require('mongoose');
-require('dotenv').config();
+const mongoose = require("mongoose");
+require("dotenv").config();
 
-exports.connect = () => {
-    mongoose.connect(progress.env.MONGODB_URL, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    })
-    .then(() => console.log("DB connected successfully"))
-    .catch((error) => {
-        console.log("DB connection is unsuccessful");
-        console.error(error);
-        process.exit(1);
-    })
-}
